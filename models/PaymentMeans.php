2788d6a922730f6c669915431e33a83fd5e763a4
David Persson <davidpersson@gmx.de>
2016-01-11 15:52:01 +0100

    Add initial payment means model.

---
+++ models/PaymentMeans.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Billing Payment
+ *
+ * Copyright (c) 2016 Atelier Disko - All rights reserved.
+ *
+ * Licensed under the AD General Software License v1.
+ *
+ * This software is proprietary and confidential. Redistribution
+ * not permitted. Unless required by applicable law or agreed to
+ * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * You should have received a copy of the AD General Software
+ * License. If not, see http://atelierdisko.de/licenses.
+ */
+
+namespace billing_payment\models;
+
+class PaymentMeans extends \base_core\models\Base {
+
+	protected $_meta = [
+		'source' => 'billing_payment_means'
+	];
+
+	protected $_actsAs = [
+		'base_core\extensions\data\behavior\RelationsPlus',
+		'base_core\extensions\data\behavior\Timestamp'
+	];
+
+	public $belongsTo = [
+		'User' => [
+			'to' => 'base_core\models\Users',
+			'key' => 'user_id'
+		]
+	];
+}
+
+?>
