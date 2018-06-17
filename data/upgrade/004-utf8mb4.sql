0e8608f44bf4de3d40ffc01394037f357627c1c0
Marius Wilms <mariuswilms@mailbox.org>
2018-06-17 21:07:23 +0200

    Include utf8mb4 migration

---
+++ data/upgrade/004-utf8mb4.sql
@@ -0,0 +1,4 @@
+ALTER TABLE billing_payments CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE billing_payments DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE billing_payment_means CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE billing_payment_means DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
